afbf4ea78d2708287f18e90f8c529f818f9a8b1d
lubian-spt <59941023+lubian-spt@users.noreply.github.com>
2022-07-12 04:57:24 -0300

    Add files via upload

---
+++ JobsDiscovery.ps1
@@ -0,0 +1,18 @@
+﻿
+#Get Veeam Jobs - In some cases, a warning appear telling that this cmdlet is not used anymore, the -warningaction removes that
+$jobs = Get-VBRJob -WarningAction SilentlyContinue
+
+#Creates the array object
+$data = New-Object System.Collections.ArrayList
+
+#Start a foreach to collect only the job name, and add to the array
+foreach($j in $jobs){
+
+$JobName = $j.Name
+
+$data += @{"jobname"=$JobName}
+
+}
+
+#Convert the array to json
+$data | ConvertTo-Json -Depth 10
